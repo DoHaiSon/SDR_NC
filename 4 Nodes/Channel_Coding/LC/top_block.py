455203ed40ef6ff31fbaf2a4aa3ab77a4147a2c0
dohaison <dohaison1998@gmail.com>
2020-12-28 08:30:04 +0000

    add_4nodes_MDC

--- 4 Nodes/Channel_Coding/LC/top_block.py
+++ 4 Nodes/Channel_Coding/LC/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Mon Dec 28 08:00:19 2020
+# Generated: Mon Dec 28 03:44:35 2020
 ##################################################
 
 if __name__ == '__main__':
@@ -16,27 +16,52 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
+from PyQt4 import Qt
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
+from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
-from grc_gnuradio import wxgui as grc_wxgui
+from math import *
 from optparse import OptionParser
 from s4a import *
 import DNC
+import DNC2
+import display
 import osmosdr
+import sip
+import sys
 import time
-import wx
+from gnuradio import qtgui
 
 
-class top_block(grc_wxgui.top_block_gui):
+class top_block(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
-        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
-        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+        gr.top_block.__init__(self, "Top Block")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Top Block")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
         ##################################################
         # Variables
@@ -47,6 +72,7 @@ class top_block(grc_wxgui.top_block_gui):
         self.pilot_block_rep = pilot_block_rep = 1
         self.occupied_tones = occupied_tones = 48
         self.fft_length = fft_length = 128
+        self.data_size_B = data_size_B = 1024
         self.data_size = data_size = 2048/2
         self.data_block_sz = data_block_sz = 7
         self.data_block_rep = data_block_rep = 1
@@ -55,9 +81,27 @@ class top_block(grc_wxgui.top_block_gui):
         ##################################################
         # Blocks
         ##################################################
+        self.show_image_1_0 = display.show_image(
+        	512,  # Picture width
+        	512,  # Picture height
+        )
+        self._show_image_1_0_win = sip.wrapinstance(self.show_image_1_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._show_image_1_0_win)
+        self.show_image_1 = display.show_image(
+        	512,  # Picture width
+        	512,  # Picture height
+        )
+        self._show_image_1_win = sip.wrapinstance(self.show_image_1.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._show_image_1_win)
         self.s4a_parallel_to_serial_1_0 = parallel_to_serial(data_block_sz*occupied_tones)
 
         self.s4a_parallel_to_serial_1 = parallel_to_serial(data_block_sz*occupied_tones)
+
+        self.s4a_ofdm_modulator_0_0 = ofdm_modulator(fft_length, occupied_tones, cp_length,
+                               preamble_block_rep, pilot_block_rep,
+                               data_block_sz, data_block_rep,
+                               silence_block_rep, 1/9.6571,
+                               1/13.9017, 1/48.0)
 
         self.s4a_ofdm_modulator_0 = ofdm_modulator(fft_length, occupied_tones, cp_length,
                                preamble_block_rep, pilot_block_rep,
@@ -77,7 +121,11 @@ class top_block(grc_wxgui.top_block_gui):
                                 data_block_rep, silence_block_rep,
                                 1000, 0, 0)
 
+        self.s4a_byte_to_bit_0_0 = byte_to_bit()
+
         self.s4a_byte_to_bit_0 = byte_to_bit()
+
+        self.s4a_bpsk_mapper_0_0 = bpsk_mapper()
 
         self.s4a_bpsk_mapper_0 = bpsk_mapper()
 
@@ -85,22 +133,9 @@ class top_block(grc_wxgui.top_block_gui):
 
         self.s4a_bpsk_demapper_0 = bpsk_demapper()
 
-        self.osmosdr_source_0_0_0 = osmosdr.source( args="numchan=" + str(1) + " " + 'bladerf=1' )
-        self.osmosdr_source_0_0_0.set_sample_rate(samp_rate)
-        self.osmosdr_source_0_0_0.set_center_freq(2.415e9, 0)
-        self.osmosdr_source_0_0_0.set_freq_corr(0, 0)
-        self.osmosdr_source_0_0_0.set_dc_offset_mode(1, 0)
-        self.osmosdr_source_0_0_0.set_iq_balance_mode(1, 0)
-        self.osmosdr_source_0_0_0.set_gain_mode(False, 0)
-        self.osmosdr_source_0_0_0.set_gain(30, 0)
-        self.osmosdr_source_0_0_0.set_if_gain(20, 0)
-        self.osmosdr_source_0_0_0.set_bb_gain(20, 0)
-        self.osmosdr_source_0_0_0.set_antenna('', 0)
-        self.osmosdr_source_0_0_0.set_bandwidth(1.5e6, 0)
-
-        self.osmosdr_source_0_0 = osmosdr.source( args="numchan=" + str(1) + " " + 'bladerf=0' )
+        self.osmosdr_source_0_0 = osmosdr.source( args="numchan=" + str(1) + " " + 'bladerf=2a45' )
         self.osmosdr_source_0_0.set_sample_rate(samp_rate)
-        self.osmosdr_source_0_0.set_center_freq(2.405e9, 0)
+        self.osmosdr_source_0_0.set_center_freq(2.435e9, 0)
         self.osmosdr_source_0_0.set_freq_corr(0, 0)
         self.osmosdr_source_0_0.set_dc_offset_mode(1, 0)
         self.osmosdr_source_0_0.set_iq_balance_mode(1, 0)
@@ -111,15 +146,38 @@ class top_block(grc_wxgui.top_block_gui):
         self.osmosdr_source_0_0.set_antenna('', 0)
         self.osmosdr_source_0_0.set_bandwidth(1.5e6, 0)
 
-        self.osmosdr_sink_0 = osmosdr.sink( args="numchan=" + str(1) + " " + 'bladerf=1' )
+        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + 'bladerf=3627' )
+        self.osmosdr_source_0.set_sample_rate(samp_rate)
+        self.osmosdr_source_0.set_center_freq(2.435e9, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(1, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(1, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
+        self.osmosdr_source_0.set_gain(30, 0)
+        self.osmosdr_source_0.set_if_gain(20, 0)
+        self.osmosdr_source_0.set_bb_gain(20, 0)
+        self.osmosdr_source_0.set_antenna('', 0)
+        self.osmosdr_source_0.set_bandwidth(1.5e6, 0)
+
+        self.osmosdr_sink_0_0 = osmosdr.sink( args="numchan=" + str(1) + " " + 'bladerf=3627' )
+        self.osmosdr_sink_0_0.set_sample_rate(samp_rate)
+        self.osmosdr_sink_0_0.set_center_freq(2.415e9, 0)
+        self.osmosdr_sink_0_0.set_freq_corr(0, 0)
+        self.osmosdr_sink_0_0.set_gain(30, 0)
+        self.osmosdr_sink_0_0.set_if_gain(20, 0)
+        self.osmosdr_sink_0_0.set_bb_gain(20, 0)
+        self.osmosdr_sink_0_0.set_antenna('', 0)
+        self.osmosdr_sink_0_0.set_bandwidth(1.5e6, 0)
+
+        self.osmosdr_sink_0 = osmosdr.sink( args="numchan=" + str(1) + " " + 'bladerf=2a45' )
         self.osmosdr_sink_0.set_sample_rate(samp_rate)
-        self.osmosdr_sink_0.set_center_freq(2.425e9, 0)
+        self.osmosdr_sink_0.set_center_freq(2.405e9, 0)
         self.osmosdr_sink_0.set_freq_corr(0, 0)
         self.osmosdr_sink_0.set_gain(30, 0)
         self.osmosdr_sink_0.set_if_gain(20, 0)
         self.osmosdr_sink_0.set_bb_gain(20, 0)
         self.osmosdr_sink_0.set_antenna('', 0)
-        self.osmosdr_sink_0.set_bandwidth(1.5e6, 0)
+        self.osmosdr_sink_0.set_bandwidth(1e6, 0)
 
         self.dc_blocker_xx_0_0_0 = filter.dc_blocker_cc(256, True)
         self.dc_blocker_xx_0_0 = filter.dc_blocker_cc(256, True)
@@ -129,22 +187,36 @@ class top_block(grc_wxgui.top_block_gui):
         self.blocks_null_sink_0_0 = blocks.null_sink(gr.sizeof_float*1)
         self.blocks_multiply_const_vxx_0_0_0_0 = blocks.multiply_const_vcc((25e3, ))
         self.blocks_multiply_const_vxx_0_0_0 = blocks.multiply_const_vcc((25e3, ))
-        self.DNC_nodeR_controller_0 = DNC.nodeR_controller(127, data_size, 6, 10, 20, 30, 40, 1, 1.0)
+        self.DNC_nodeB_controller_0 = DNC.nodeB_controller(127, data_size_B, 6, 10, 20, 30, 40)
+        self.DNC_nodeA_controller_0 = DNC.nodeA_controller(127, data_size, 6, 10, 20, 30, 40)
+        self.DNC2_image_vector_source_0_0_0 = DNC2.image_vector_source('/home/ubuntu/Desktop/SDR_NC/2 Nodes/2_P2P_Img/livingroom_gray_512.txt', 512*512, data_size_B, False, 1, 1, [])
+        self.DNC2_image_vector_source_0_0 = DNC2.image_vector_source('/home/ubuntu/Desktop/SDR_NC/2 Nodes/2_P2P_Img/lena_gray_512.txt', 512*512, data_size, False, 1, 1, [])
+        self.DNC2_hamming_encoder_1_0 = DNC2.hamming_encoder(3, 512*512, data_size_B)
+        self.DNC2_hamming_encoder_1 = DNC2.hamming_encoder(3, 512*512, data_size)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.DNC_nodeR_controller_0, 0), (self.s4a_byte_to_bit_0, 0))
+        self.connect((self.DNC2_hamming_encoder_1, 0), (self.DNC_nodeA_controller_0, 1))
+        self.connect((self.DNC2_hamming_encoder_1_0, 0), (self.DNC_nodeB_controller_0, 1))
+        self.connect((self.DNC2_image_vector_source_0_0, 0), (self.DNC2_hamming_encoder_1, 0))
+        self.connect((self.DNC2_image_vector_source_0_0, 0), (self.show_image_1, 0))
+        self.connect((self.DNC2_image_vector_source_0_0_0, 0), (self.DNC2_hamming_encoder_1_0, 0))
+        self.connect((self.DNC2_image_vector_source_0_0_0, 0), (self.show_image_1_0, 0))
+        self.connect((self.DNC_nodeA_controller_0, 0), (self.s4a_byte_to_bit_0, 0))
+        self.connect((self.DNC_nodeB_controller_0, 0), (self.s4a_byte_to_bit_0_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0_0_0, 0), (self.s4a_ofdm_demodulator_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0_0_0_0, 0), (self.s4a_ofdm_demodulator_0_0, 0))
         self.connect((self.dc_blocker_xx_0_0, 0), (self.blocks_multiply_const_vxx_0_0_0, 0))
         self.connect((self.dc_blocker_xx_0_0_0, 0), (self.blocks_multiply_const_vxx_0_0_0_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.dc_blocker_xx_0_0_0, 0))
         self.connect((self.osmosdr_source_0_0, 0), (self.dc_blocker_xx_0_0, 0))
-        self.connect((self.osmosdr_source_0_0_0, 0), (self.dc_blocker_xx_0_0_0, 0))
-        self.connect((self.s4a_bpsk_demapper_0, 0), (self.DNC_nodeR_controller_0, 0))
-        self.connect((self.s4a_bpsk_demapper_0_0, 0), (self.DNC_nodeR_controller_0, 1))
+        self.connect((self.s4a_bpsk_demapper_0, 0), (self.DNC_nodeA_controller_0, 0))
+        self.connect((self.s4a_bpsk_demapper_0_0, 0), (self.DNC_nodeB_controller_0, 0))
         self.connect((self.s4a_bpsk_mapper_0, 0), (self.s4a_ofdm_modulator_0, 0))
+        self.connect((self.s4a_bpsk_mapper_0_0, 0), (self.s4a_ofdm_modulator_0_0, 0))
         self.connect((self.s4a_byte_to_bit_0, 0), (self.s4a_bpsk_mapper_0, 0))
+        self.connect((self.s4a_byte_to_bit_0_0, 0), (self.s4a_bpsk_mapper_0_0, 0))
         self.connect((self.s4a_ofdm_demodulator_0, 2), (self.blocks_null_sink_0_0, 0))
         self.connect((self.s4a_ofdm_demodulator_0, 1), (self.blocks_null_sink_0_1, 0))
         self.connect((self.s4a_ofdm_demodulator_0, 0), (self.s4a_parallel_to_serial_1, 0))
@@ -152,8 +224,14 @@ class top_block(grc_wxgui.top_block_gui):
         self.connect((self.s4a_ofdm_demodulator_0_0, 1), (self.blocks_null_sink_0_1_0, 0))
         self.connect((self.s4a_ofdm_demodulator_0_0, 0), (self.s4a_parallel_to_serial_1_0, 0))
         self.connect((self.s4a_ofdm_modulator_0, 0), (self.osmosdr_sink_0, 0))
+        self.connect((self.s4a_ofdm_modulator_0_0, 0), (self.osmosdr_sink_0_0, 0))
         self.connect((self.s4a_parallel_to_serial_1, 0), (self.s4a_bpsk_demapper_0, 0))
         self.connect((self.s4a_parallel_to_serial_1_0, 0), (self.s4a_bpsk_demapper_0_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
 
     def get_silence_block_rep(self):
         return self.silence_block_rep
@@ -166,8 +244,9 @@ class top_block(grc_wxgui.top_block_gui):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.osmosdr_source_0_0_0.set_sample_rate(self.samp_rate)
         self.osmosdr_source_0_0.set_sample_rate(self.samp_rate)
+        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
+        self.osmosdr_sink_0_0.set_sample_rate(self.samp_rate)
         self.osmosdr_sink_0.set_sample_rate(self.samp_rate)
 
     def get_preamble_block_rep(self):
@@ -193,6 +272,12 @@ class top_block(grc_wxgui.top_block_gui):
 
     def set_fft_length(self, fft_length):
         self.fft_length = fft_length
+
+    def get_data_size_B(self):
+        return self.data_size_B
+
+    def set_data_size_B(self, data_size_B):
+        self.data_size_B = data_size_B
 
     def get_data_size(self):
         return self.data_size
@@ -221,9 +306,21 @@ class top_block(grc_wxgui.top_block_gui):
 
 def main(top_block_cls=top_block, options=None):
 
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
     tb = top_block_cls()
-    tb.Start(True)
-    tb.Wait()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
 
 
 if __name__ == '__main__':
