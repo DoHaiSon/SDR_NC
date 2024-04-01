36d9309d9e30cd986fbf4626b536c3ff5d424c67
mailoan <loanuet2210@gmail.com>
2024-04-01 09:54:58 +0700

    Update convolution

--- Software/GnuRadio_Block_NC/gr-DNC2/lib/convolutional_decoder_impl.cc
+++ Software/GnuRadio_Block_NC/gr-DNC2/lib/convolutional_decoder_impl.cc
@@ -31,225 +31,197 @@
 #include <time.h>
 #include <sys/time.h>
 
-namespace gr {
-  namespace DNC2 {
+namespace gr
+{
+namespace DNC2
+{
 
-    convolutional_decoder::sptr
-    convolutional_decoder::make(const std::vector<int> gen1,
-			const std::vector<int> gen2)
-    {
-      return gnuradio::get_initial_sptr
-        (new convolutional_decoder_impl(gen1, gen2));
-    }
+convolutional_decoder::sptr
+convolutional_decoder::make(const std::vector<int> gen1,
+							const std::vector<int> gen2)
+{
+	return gnuradio::get_initial_sptr(new convolutional_decoder_impl(gen1, gen2));
+}
 
-    /*
+/*
      * The private constructor
      */
-    convolutional_decoder_impl::convolutional_decoder_impl(const std::vector<int> gen1,
-			const std::vector<int> gen2)
-      : gr::block("convolutional_decoder",
-              gr::io_signature::make(1, 1, sizeof(char)),
-              gr::io_signature::make(1, 1, sizeof(char))),
-	      d_gen1(gen1),
-	      d_gen2(gen2),
-	      generator1(0x7),
-	      generator2(0x5)
-    {
-	aa.resize(8); std::fill(aa.begin(), aa.end(), 0);
-	ac.resize(8); std::fill(ac.begin(), ac.end(), 0);
-	ba.resize(8); std::fill(ba.begin(), ba.end(), 0);
-	bc.resize(8); std::fill(bc.begin(), bc.end(), 0);
-	cb.resize(8); std::fill(cb.begin(), cb.end(), 0);
-	cd.resize(8); std::fill(cd.begin(), cd.end(), 0);
-	db.resize(8); std::fill(db.begin(), db.end(), 0);
-	dd.resize(8); std::fill(dd.begin(), dd.end(), 0);
-	
-	Ma.resize(8); std::fill(Ma.begin(), Ma.end(), 0);
-	Mb.resize(8); std::fill(Mb.begin(), Mb.end(), 0);
-	Mc.resize(8); std::fill(Mc.begin(), Mc.end(), 0);
-	Md.resize(8); std::fill(Md.begin(), Md.end(), 0);
+convolutional_decoder_impl::convolutional_decoder_impl(const std::vector<int> gen1,
+													   const std::vector<int> gen2)
+	: gr::block("convolutional_decoder",
+				gr::io_signature::make(1, 1, sizeof(char)),
+				gr::io_signature::make(1, 1, sizeof(char))),
+	  d_gen1(gen1),
+	  d_gen2(gen2),
+	  generator1(0x7),
+	  generator2(0x5)
+{
+}
 
-	Aa.resize(8); std::fill(Aa.begin(), Aa.end(), 0);
-	Ab.resize(8); std::fill(Ab.begin(), Ab.end(), 0);
-	Ac.resize(8); std::fill(Ac.begin(), Ac.end(), 0);
-	Ad.resize(8); std::fill(Ad.begin(), Ad.end(), 0);
-    }
-
-    /*
+/*
      * Our virtual destructor.
      */
-    convolutional_decoder_impl::~convolutional_decoder_impl()
-    {
-    }
+convolutional_decoder_impl::~convolutional_decoder_impl()
+{
+}
 
-    void
-    convolutional_decoder_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
-    {
-        ninput_items_required[0] = noutput_items*2;
-    }
+void convolutional_decoder_impl::forecast(int noutput_items, gr_vector_int &ninput_items_required)
+{
+	ninput_items_required[0] = noutput_items * 2;
+}
 
-    int
-    convolutional_decoder_impl::general_work (int noutput_items,
-                       gr_vector_int &ninput_items_v,
-                       gr_vector_const_void_star &input_items,
-                       gr_vector_void_star &output_items)
-    {
-        const char *in = (const char *) input_items[0];
-        char *out = (char *) output_items[0];
+int convolutional_decoder_impl::general_work(int noutput_items,
+											 gr_vector_int &ninput_items_v,
+											 gr_vector_const_void_star &input_items,
+											 gr_vector_void_star &output_items)
+{
+	static bool done = false;
+	if (done)
+	{
+		return -1;
+	}
+	const char *in = (const char *)input_items[0];
+	char *out = (char *)output_items[0];
 	int ninput_items = ninput_items_v[0];
-	
+	int outputs_[8];
+
 	int ni = 0, no = 0;
-	while (ni<ninput_items && no<noutput_items) {
-		out[no] = decode(in[ni],in[ni+1],generator1,generator2); //printf("%d\n",out[no]);
-		ni += 2;no ++;
-	}
-
-        consume_each (ni);
-        return no;
-    }
-
-char convolutional_decoder_impl::decode(char code1,char code2, char gen1, char gen2) {
-	char rcode[1024];
-	for (int i=0; i<8; i++)  {
-		rcode[i] = ((code1 >> (7-i)) & 0x1);
-		rcode[i+8] = ((code2 >> (7-i)) & 0x1);
-	}
-	//printf("rcode: ");for (int i=0; i<16; i++)  {printf("%d",rcode[i]);} printf("\n");
-	/*calculate all branch metric*/
-	aa = branch_metric_calculation(rcode, 0x0); 
-	ac = branch_metric_calculation(rcode, 0x3);
-	ba = ac;
-	bc = aa;
-	cb = branch_metric_calculation(rcode, 0x2);
-	cd = branch_metric_calculation(rcode, 0x1);
-	db = cd;
-	dd = cb;
-	ba[0] = 0; ba[1] = 0;
-    	bc[0] = 0; bc[1] = 0;
-    	db[0] = 0; db[1] = 0;
-    	dd[0] = 0; dd[1] = 0;
-    	cb[0] = 0;
-    	cd[0] = 0;
-	/*print_vector(aa,8);
-	print_vector(ac,8);
-	print_vector(ba,8);
-	print_vector(bc,8);
-	print_vector(cb,8);
-	print_vector(cd,8);
-	print_vector(db,8);
-	print_vector(dd,8);
-	/*t = 0. Suviving metric*/
-	Ma[0] = aa[0];
-    	Mb[0] = 0;
-    	Mc[0] = ac[0];
-    	Md[0] = 0;
-	/*initial trace*/
-	Aa[0] = 1;  
-    	Ab[0] = 0;  
-    	Ac[0] = 1; 
-    	Ad[0] = 0; 
-	/*t = 1. Surviving metric*/
-	Ma[1] = Ma[0] + aa[1];
-    	Mb[1] = Mc[0] + cb[1];
-    	Mc[1] = Ma[0] + ac[1];
-    	Md[1] = Mc[0] + cd[1];
-	/*trace*/
-	Aa[1] = 1;
-    	Ab[1] = 3;
-    	Ac[1] = 1;
-    	Ad[1] = 3;
-	/*Continous surviving metric and trace*/
-		for (int i=2;i<8;i++) {  
-       			//Metric at a
-        		if (  Ma[i-1]+aa[i] > Mb[i-1]+ba[i]  ) {
-            			Aa[i] = 2;
-            			Ma[i] = Mb[i-1]+ba[i];
-			}
-        		else {
-            			Aa[i] = 1;
-            			Ma[i] = Ma[i-1]+aa[i];
-			}
-        		//Metric at b
-        		if ( Mc[i-1] + cb[i] > Md[i-1] +db[i] ) {
-            			Ab[i] = 4;
-            			Mb[i] = Md[i-1] + db[i];
-			}
-       	 		else {
-            			Ab[i] = 3;
-            			Mb[i] = Mc[i-1] + cb[i];
-			}
-        		//Metric at c
-        		if (Ma[i-1] + ac[i] > Mb[i-1] + bc[i] ) {
-            			Ac[i] = 2;
-            			Mc[i] = Mb[i-1] + bc[i];
-			}
-        		else {
-            			Ac[i] = 1;
-            			Mc[i] = Ma[i-1] + ac[i];
-			}
-        		//Metric at d
-        		if (Mc[i-1] + cd[i] > Md[i-1]+dd[i] ) {
-            			Ad[i] = 4;
-            			Md[i] = Md[i-1] + dd[i];
-			}
-        		else {
-            			Ad[i] = 3;
-            			Md[i] = Mc[i-1] + cd[i];
-			}
-		}
-	/*printf("Aa: "); for (int i = 1;i<8;i++) printf ("%i ",Aa[i]); printf("\n");
-	printf("Ab: "); for (int i = 1;i<8;i++) printf ("%i ",Ab[i]); printf("\n");
-	printf("Ac: "); for (int i = 1;i<8;i++) printf ("%i ",Ac[i]); printf("\n");
-	printf("Ad: "); for (int i = 1;i<8;i++) printf ("%i ",Ad[i]); printf("\n");
-	//printf("Ma: %i, Mb: %i, Mc: %i, Md: %i\n", Ma[7], Mb[7], Mc[7], Md[7]);
-	/*Decide shortest path and corresponding trace*/
-	int Mmin = Ma[7]; int index = 1;
-	if (Mb[7]<Mmin) { index = 2; Mmin = Mb[7];}
-	if (Mc[7]<Mmin) { index = 3; Mmin = Mc[7];}
-	if (Md[7]<Mmin) { index = 4; Mmin = Md[7];};
-	//printf("Ma: %i, Mb: %i, Mc: %i, Md: %i, Mmin: %i\n", Ma[7], Mb[7], Mc[7], Md[7], Mmin);
-	/*Trace back to estimate message*/
-	char emsg=0x0;
-	//printf("path: ");printf("%i ",index);
-	for (int i = 7;i>=0;i--) {
-        	int tmpIndex = index; 
-        	if (index == 1) index = Aa[i];
-		else if (index == 2)  index = Ab[i]; 
-		else if (index == 3)  index = Ac[i]; 
-		else if (index == 4)  index = Ad[i]; 
-		//printf("%i ",index); //change index; 
-        	if ( ( (tmpIndex == 1) && (index == 1) )||((tmpIndex == 1) && (index == 2)) )
-            		emsg |= (0x0 <<(7-i));
-        	else if ( (tmpIndex == 2) && (index == 3)||(tmpIndex == 2) && (index == 4) )
-            		emsg |= (0x0 <<(7-i));
-        	else if ( (tmpIndex == 3) && (index == 1) ||(tmpIndex == 3) && (index == 2)) 
-            		emsg |= (0x1 <<(7-i));
-        	else if ( (tmpIndex == 4) && (index == 3)||(tmpIndex == 4) && (index == 4) ) 
-            		emsg |= (0x1 <<(7-i));
-        }
-	//printf("\n"); printf("emsg: "); for (int i = 0;i<8;i++) {printf("%d",(emsg>> (7-i)) & 0x1);}; printf("\n");
-        return emsg;
+	int **mapStateInput = initializeOutputs();
+	std::string a = decode(in, ninput_items, mapStateInput);
+	std::cout << "test:" << a;
+	consume_each(ni);
+	done = true;
+	return no;
 }
 
-std::vector<int> convolutional_decoder_impl::branch_metric_calculation(char code[], char value) {
-	std::vector<int> metric; metric.resize(8); std::fill(metric.begin(), metric.end(), 0);
-	char d_value[1024]; 
-	d_value[0] = (value >> 1) & 0x1; d_value[1] = value & 0x1;
-	//printf("rcode in function: ");for (int i=0; i<16; i++)  {printf("%d",code[i]);} printf("\n");	
-	for (int i=0;i<8;i++) {
-		for (int j=0;j<2;j++) { 		
-			if ((code[2*i+j] & 0x1) != (d_value[j]&0x1)) metric[i] ++; 
-			//printf("%d__%d ",(code[2*i+j] & 0x1),(d_value[j]&0x1));
+int **convolutional_decoder_impl::initializeOutputs()
+{
+	int **mapStateInput = new int *[4];
+	for (int i = 0; i < 4; i++)
+	{
+		mapStateInput[i] = new int[2];
+		for (int j = 0; j < 2; j++)
+		{
+			int xn2 = i & 1;
+			int xn1 = (i >> 1) & 1;
+			int codebit1 = (0 * 1 + xn2 * 1 + xn1 * 1) % 2;
+			int codebit2 = (0 * 1 + xn2 * 0 + xn1 * 1) % 2;
+			mapStateInput[i][j] = (codebit1 << 1) | codebit2;
 		}
 	}
-	//printf("\n");	
-	//printf("Metric: ");for (int i=0; i<8; i++)  {printf("%d ",metric[i]);} printf("\n");	
-	return metric;
+	return mapStateInput;
 }
- 
-void convolutional_decoder_impl::print_vector(std::vector<int> x, int length) {
-	for (int i=0; i<length; i++)  {printf("%d ",x[i]);}; 
-	printf("\n");	
+
+int convolutional_decoder_impl::hammingDistance(int x, int y)
+{
+	int dist = 0;
+	for (int i = 0; i < 2; ++i)
+	{
+		int bitX = (x >> i) & 1;
+		int bitY = (y >> i) & 1;
+		if (bitX != bitY)
+		{
+			++dist;
+		}
+	}
+
+	return dist;
 }
-  } /* namespace DNC2 */
+
+int convolutional_decoder_impl::branchMetric(int received_bit,
+											 int current_state,
+											 int input,
+											 int **mapStateInput)
+{
+	int output = mapStateInput[current_state][input];
+	return hammingDistance(received_bit, output);
+}
+
+std::pair<int, int> convolutional_decoder_impl::pathMetric(
+	int received_bit,
+	const std::vector<int> &prev_path_metrics,
+	int input, int **mapStateInput)
+{
+	int s = (input & ((1 << (3 - 2)) - 1)) << 1;
+	int source_state1 = s | 0;
+	int source_state2 = s | 1;
+
+	int pm1 = prev_path_metrics[source_state1];
+	if (pm1 < std::numeric_limits<int>::max())
+	{
+		pm1 += branchMetric(received_bit, source_state1, input, mapStateInput);
+	}
+	int pm2 = prev_path_metrics[source_state2];
+	if (pm2 < std::numeric_limits<int>::max())
+	{
+		pm2 += branchMetric(received_bit, source_state2, input, mapStateInput);
+	}
+
+	if (pm1 <= pm2)
+	{
+		return std::make_pair(pm1, source_state1);
+	}
+	else
+	{
+		return std::make_pair(pm2, source_state2);
+	}
+}
+
+typedef std::vector<std::vector<int> > Trellis;
+
+void convolutional_decoder_impl::updatePathMetrics(int received_bit,
+												   std::vector<int> *path_metrics,
+												   Trellis * trellis,
+												   int **mapStateInput)
+{
+	std::vector<int> new_path_metrics(path_metrics->size());
+	std::vector<int> new_trellis_column(1 << (3 - 1));
+	for (int i = 0; i < path_metrics->size(); i++)
+	{
+		std::pair<int, int> p = pathMetric(received_bit, *path_metrics, i, mapStateInput);
+		new_path_metrics[i] = p.first;
+		new_trellis_column[i] = p.second;
+	}
+
+	*path_metrics = new_path_metrics;
+	trellis->push_back(new_trellis_column);
+}
+
+std::string convolutional_decoder_impl::decode(const char *msg, int nin, int **mapStateInput)
+{
+	// Compute path metrics and generate trellis.
+	std::vector<std::vector<int> > trellis;
+	std::vector<int> path_metrics(1 << (3 - 1),
+								  std::numeric_limits<int>::max());
+	path_metrics.front() = 0;
+	for (int i = 0; i < nin; i += 2)
+	{
+		std::string current_bits(msg, i, 2);
+		// If some bits are missing, fill with trailing zeros.
+		// This is not ideal but it is the best we can do.
+		if (current_bits.size() < 2)
+		{
+			current_bits.append(
+				std::string(2 - current_bits.size(), '0'));
+		}
+		int current_int = std::atoi(current_bits.c_str());
+		updatePathMetrics(current_int, &path_metrics, &trellis, mapStateInput);
+	}
+
+	// Traceback.
+	std::string decoded;
+	int state = std::min_element(path_metrics.begin(), path_metrics.end()) -
+				path_metrics.begin();
+	for (int i = trellis.size() - 1; i >= 0; i--)
+	{
+		decoded += state >> (3 - 2) ? "1" : "0";
+		state = trellis[i][state];
+	}
+	std::reverse(decoded.begin(), decoded.end());
+
+	// Remove (constraint_ - 1) flushing bits.
+	return decoded.substr(0, decoded.size() - 3 + 1);
+}
+
+} /* namespace DNC2 */
 } /* namespace gr */
--- Software/GnuRadio_Block_NC/gr-DNC2/lib/convolutional_encoder_impl.cc
+++ Software/GnuRadio_Block_NC/gr-DNC2/lib/convolutional_encoder_impl.cc
@@ -16,95 +16,99 @@
 #include <time.h>
 #include <sys/time.h>
 
-namespace gr {
-  namespace DNC2 {
+namespace gr
+{
+namespace DNC2
+{
 
-    convolutional_encoder::sptr
-    convolutional_encoder::make(const std::vector<int> gen1,
-			const std::vector<int> gen2)
-    {
-      return gnuradio::get_initial_sptr
-        (new convolutional_encoder_impl(gen1, gen2));
-    }
+convolutional_encoder::sptr
+convolutional_encoder::make(const std::vector<int> gen1,
+                            const std::vector<int> gen2)
+{
+    return gnuradio::get_initial_sptr(new convolutional_encoder_impl(gen1, gen2));
+}
 
-    /*
+/*
      * The private constructor
      */
-    convolutional_encoder_impl::convolutional_encoder_impl(const std::vector<int> gen1,
-			const std::vector<int> gen2)
-      : gr::block("convolutional_encoder",
-              gr::io_signature::make(1, 1, sizeof(char)),
-              gr::io_signature::make(1, 1, sizeof(char))),
-	      d_gen1(gen1),
-	      d_gen2(gen2),
-	      generator1(0x7),
-	      generator2(0x5)
+
+convolutional_encoder_impl::convolutional_encoder_impl(const std::vector<int> gen1,
+                                                       const std::vector<int> gen2)
+    : gr::block("convolutional_encoder",
+                gr::io_signature::make(1, 1, sizeof(char)),
+                gr::io_signature::make(1, 1, sizeof(char))),
+      d_gen1(gen1),
+      d_gen2(gen2),
+      generator1(0x7),
+      generator2(0x5)
+{
+    char generator1 = 0x7;
+    char generator2 = 0x5;
+}
+
+convolutional_encoder_impl::~convolutional_encoder_impl()
+{
+}
+
+void convolutional_encoder_impl::forecast(int noutput_items, gr_vector_int &ninput_items_required)
+{
+    ninput_items_required[0] = noutput_items / 2;
+}
+
+int convolutional_encoder_impl::general_work(int noutput_items,
+                                             gr_vector_int &ninput_items_v,
+                                             gr_vector_const_void_star &input_items,
+                                             gr_vector_void_star &output_items)
+{
+    static bool done = false;
+    if (done)
     {
-	char generator1 = 0x7;//(d_gen1[1] | (d_gen1[2]<<1)) | (d_gen1[3]<<2); 
-	//std::cout<<gen1[1]<<"_"<<gen1[2]<<"_"<<gen1[3]<<"_"<<(int)generator1<<std::endl;
-	char generator2 = 0x5;//(d_gen2[1] | (d_gen2[2]<<1)) | (d_gen2[3]<<2); 
-	//std::cout<<d_gen2[1]<<"_"<<d_gen2[2]<<"_"<<d_gen2[3]<<"_"<<(int)generator2<<std::endl;
+        return -1;
     }
-
-    /*
-     * Our virtual destructor.
-     */
-    convolutional_encoder_impl::~convolutional_encoder_impl()
+    const char *in = (const char *)input_items[0];
+    char *out = (char *)output_items[0];
+    int ninput_items = ninput_items_v[0];
+    int ni = 0;
+    int no = 0;
+    int resgister[2] = {0, 0};
+    char code1, code2;
+    printf("Loan test in: \n");
+    while (ni < ninput_items)
     {
+        printf("%d ", in[ni]);
+        out[no] = (in[ni] + resgister[0] + resgister[1]) % 2;
+        out[no + 1] = (in[ni] + resgister[0] * 0 + resgister[1]) % 2;
+        resgister[1] = resgister[0];
+        resgister[0] = in[ni];
+        ni++;
+        no += 2;
     }
 
-    void
-    convolutional_encoder_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
+    printf("\nLoan test out\n");
+    for (int i = 0; i < no; i++)
     {
-        ninput_items_required[0] = noutput_items/2;
+        printf(" %d ", out[i]);
     }
+    consume_each(ni);
+    done = true;
+    return no;
+}
 
-    int
-    convolutional_encoder_impl::general_work (int noutput_items,
-                       gr_vector_int &ninput_items_v,
-                       gr_vector_const_void_star &input_items,
-                       gr_vector_void_star &output_items)
+char convolutional_encoder_impl::convolution(char msg, char gen)
+{
+    char codeword = 0x0;
+    char tmp;
+    for (int i = 0; i < 8; i++)
     {
-        const char *in = (const char *) input_items[0];
-        char *out = (char *) output_items[0];
-	int ninput_items = ninput_items_v[0];
-
-        int ni=0; int no=0;
-	char code1, code2;
-	while (ni<ninput_items && no<noutput_items) {
-		char code1[1024], code2[1024], conv1, conv2, out1, out2;
-		conv1 = convolution(in[ni],generator1);
-		conv2 = convolution(in[ni],generator2); //printf("%d_%d\n",conv1, conv2);
-		for (int i=0; i<8; i++)  code1[i] = ((conv1 >> (7-i)) & 0x1); //for (int i = 0;i<8;i++) {printf("%d",code1[i]);} printf("\n");
-		for (int i=0; i<8; i++)  code2[i] = ((conv2 >> (7-i)) & 0x1); //for (int i = 0;i<8;i++) {printf("%d",code2[i]);} printf("\n\n");
-		for (int i = 0;i<4;i++) {
-			out[no] |= (((code1[i]) & 0x1)<<(7-2*i)); //printf("%d_",(((code1[i]) & 0x1)<<(2*i)));
-			out[no] |= (((code2[i]) & 0x1)<<(6-2*i)); //printf("%d__",(((code2[i]) & 0x1)<<(2*i+1))); 
-			out[no+1] |= (((code1[i+4]) & 0x1)<<(7-2*i)); //printf("%d_",(((code1[i+4]) & 0x1)<<(2*i)));
-			out[no+1] |= (((code2[i+4]) & 0x1)<<(6-2*i)); //printf("%d\n",(((code2[i+4]) & 0x1)<<(2*i+1)));
-		}
-		//out[no]	= out1; out[no+1] = out2;
-		//for (int i = 0;i<8;i++) {printf("%d",(out[no]>> (7-i)) & 0x1);} 
- 		//for (int i = 0;i<8;i++) {printf("%d",(out[no+1]>> (7-i)) & 0x1);} printf("\n");	
-		ni ++; no += 2; 
-	}
-        consume_each (ni);      
-        return no;
+        tmp = 0x0;
+        for (int j = 0; j < 3; j++)
+        {
+            tmp ^= (((msg) >> (i + j)) & (gen >> j));
+        }
+        codeword |= ((tmp & 0x1) << i);
     }
+    return codeword;
+}
 
-    char convolutional_encoder_impl::convolution(char msg, char gen){
-	char codeword = 0x0;
-	char tmp;
-	for (int i = 0;i<8;i++) {
-		tmp = 0x0;
-		for (int j = 0;j<3;j++) {
-			tmp ^= (((msg)>>(i+j))&(gen>>j)); //printf("tmp = %d\n",((msg<<2)>>(i+j) & 0x1));
-		}
-		codeword |= ((tmp & 0x1)<<i);   	
-	}
-	return codeword;
-    }
-
-  } /* namespace DNC2 */
+} /* namespace DNC2 */
 } /* namespace gr */
-
